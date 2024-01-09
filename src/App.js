8720d4387e6b195689c61cce7e3c88e2d35e344e
aman-gupta-32 <68238362+aman-gupta-32@users.noreply.github.com>
2024-01-09 23:35:20 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -100,7 +100,7 @@ function FriendsList({ friends, onSelection, selectedFriend }) {
 function Nav() {
   return (
     <div className="nav-bar">
-      <img src={process.env.PUBLIC_URL + '/icon.svg'} alt="Not Found" />
+      <img src="/icon.svg" alt="Not Found" />
     </div>
   );
 }
